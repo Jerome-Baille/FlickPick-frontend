5d80e3301aa788ba8a3bf26800c472992e2548c0
Jerome-Baille <baille.jerome@gmail.com>
2025-03-05 11:35:49 +0100

    Add production environment configuration with API URLs for authentication and media services

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+const authBaseURL = 'https://auth.jerome-baille.fr/api';
+const flickPickBaseURL = 'https://flick-pick.jerome-baille.fr/api';
+const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original';
+const TMDB_IMAGE_BASE_URL_300 = 'https://image.tmdb.org/t/p/w300';
+const TMDB_BASE_API_URL = "https://api.themoviedb.org/3";
+
+export const environment = {
+  production: true,
+  authBaseURL,
+  authURL: `${authBaseURL}/auth`,
+  userURL: `${flickPickBaseURL}/user`,
+  groupURL: `${flickPickBaseURL}/group`,
+  listURL: `${flickPickBaseURL}/list`,
+  mediaURL: `${flickPickBaseURL}/media`,
+  voteURL: `${flickPickBaseURL}/vote`,
+  tmdbUrl: `${flickPickBaseURL}/tmdb`,
+  TMDB_IMAGE_BASE_URL,
+  TMDB_IMAGE_BASE_URL_300,
+  TMDB_BASE_API_URL
+};
